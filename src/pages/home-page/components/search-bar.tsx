dc39c8d1d0bf486d427b188ebd6dd488e774ab51
Deepen Timalsina <deepentimalsina05@gmail.com>
2025-07-25 08:34:53 +0545

    update: search-bar to use cva for variants

--- src/pages/home-page/components/search-bar.tsx
+++ src/pages/home-page/components/search-bar.tsx
@@ -3,38 +3,49 @@ import { Input } from '@/components/ui/input';
 import Button from '@/components/ui/button';
 import { useSearchBarValues } from '../hooks/useSearchBarValues';
 import { cn } from '@/lib/utils';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-export interface ISearchBarProps {
+const searchBarVariants = cva(
+  'px-2 flex items-center justify-between gap-x-3 bg-search-bar-bg',
+  {
+    variants: {
+      rounded: {
+        xl: 'rounded-xl',
+        '2xl': 'rounded-2xl',
+      },
+      variant: {
+        small: 'py-[0.05rem]',
+        large: 'py-[0.4rem]',
+      },
+    },
+    defaultVariants: {
+      rounded: 'xl',
+      variant: 'large',
+    },
+  }
+);
+
+export interface ISearchBarProps
+  extends VariantProps<typeof searchBarVariants> {
   initialValue: string;
   onSearch: (search: string) => void;
   showButton?: boolean;
   placeholder?: string;
-  rounded?: 'xl' | '2xl';
-  variant?: 'small' | 'large';
   className?: string;
 }
 
 function SearchBar({
   showButton = true,
   placeholder = 'Type to search...',
-  rounded = 'xl',
-  variant = 'large',
+  rounded,
+  variant,
   className = '',
   ...props
 }: ISearchBarProps) {
   const { searchValue, handleSearch } = useSearchBarValues(props);
 
   return (
-    <div
-      className={cn(
-        'px-2 flex items-center justify-between gap-x-3 bg-search-bar-bg',
-        rounded === 'xl' && 'rounded-xl',
-        rounded === '2xl' && 'rounded-2xl',
-        variant === 'small' && 'py-[0.05rem]',
-        variant === 'large' && 'py-[0.4rem]',
-        className
-      )}
-    >
+    <div className={cn(searchBarVariants({ rounded, variant }), className)}>
       <div className="w-full p-1 flex items-center">
         <Search className="text-gray-400 w-[1.1rem] aspect-square" />
         <Input
@@ -45,7 +56,6 @@ function SearchBar({
           className="flex-1 w-full bg-transparent border-none text-white placeholder:text-muted-text placeholder:text-[0.9rem]"
         />
       </div>
-
       {showButton && (
         <Button className="bg-blue font-semibold rounded-lg h-10">
           Search
